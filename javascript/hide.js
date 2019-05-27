b10d51b9c8c3d14e9074b73016c26059bec456c8
LINKER <temp2temp@daum.net>
2019-05-27 16:37:49 +0900

    Add files via upload

---
+++ javascript/hide.js
@@ -0,0 +1,23 @@
+function login()
+{
+    document.getElementById("login_hide").style.display = "none";
+}
+
+function open_1()
+{
+    document.getElementById("hide_1").style.display = "block";
+    document.getElementById("ca_open_1").style.padding = "5%";
+    document.getElementById("ca_open_2").style.paddingBottom = "40px";
+}
+function open_2()
+{
+    document.getElementById("hide_2").style.display = "block";
+    document.getElementById("ca_open_1").style.padding = "5%";
+    document.getElementById("ca_open_2").style.paddingBottom = "40px";
+}
+function open_3()
+{
+    document.getElementById("hide_3").style.display = "block";
+    document.getElementById("ca_open_1").style.padding = "5%";
+    document.getElementById("ca_open_2").style.paddingBottom = "40px";
+}
